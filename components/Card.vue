82d5189ffac89157e45d7ad75d5e99f41bd59b01
unknown <cahuzac.p@gmail.com>
2022-06-30 20:19:33 +0200

    first commit

---
+++ components/Card.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="card">
+    CARD
+    <button @click="asyncData">AsyncData</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => {
+    return {};
+  },
+  methods: {
+    async asyncData() {
+      const data = await this.$axios.$get(
+        "https://superheroapi.com/api/2567746080028499/70/powerstats"
+      );
+      console.log(data);
+      return { ip };
+    },
+  },
+  name: "Card",
+};
+</script>
+
+<style>
+@media screen and(max-width: 400px) {
+  .card {
+    width: 80%;
+    height: auto;
+    border: solid 2px grey;
+    margin: 5px;
+  }
+}
+.card {
+  width: 180px;
+  height: 250px;
+  border: solid 2px grey;
+  margin: 5px;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="card-container">
+    <Card />
+    <Card /><Card /><Card /> <Card /><Card /> <Card /> <Card /><Card />
+    <Card /><Card /><Card />
+    <Card />
+    <Card /><Card /><Card />
+  </div>
+</template>
+
+<script>
+import Card from "@/components/Card.vue";
+export default {
+  name: "IndexPage",
+};
+</script>
+<style>
+.card-container {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  min-height: 100vh;
+}
+</style>
